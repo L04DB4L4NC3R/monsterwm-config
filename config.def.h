87534d32bb3830bbe253f14907c77d8ca339287f
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-04-27 23:35:16 +0530

    center, fib mode, useless gaps etc

--- config.def.h
+++ config.def.h
@@ -16,8 +16,8 @@
 #define PANEL_HEIGHT    18        /* 0 for no space for panel, thus no panel */
 #define DEFAULT_MODE    TILE      /* initial layout/mode: TILE MONOCLE BSTACK GRID FLOAT */
 #define ATTACH_ASIDE    True      /* False means new window is master */
-#define FOLLOW_WINDOW   False     /* follow the window when moved to a different desktop */
-#define FOLLOW_MOUSE    False     /* focus the window the mouse just entered */
+#define FOLLOW_WINDOW   True     /* follow the window when moved to a different desktop */
+#define FOLLOW_MOUSE    True     /* focus the window the mouse just entered */
 #define CLICK_TO_FOCUS  True      /* focus an unfocused window when clicked  */
 #define FOCUS_BUTTON    Button3   /* mouse button to be used along with CLICK_TO_FOCUS */
 #define BORDER_WIDTH    2         /* window border width */
@@ -26,7 +26,7 @@
 #define MINWSZ          50        /* minimum window size in pixels  */
 #define DEFAULT_DESKTOP 0         /* the desktop to focus initially */
 #define DESKTOPS        4         /* number of desktops - edit DESKTOPCHANGE keys to suit */
-#define USELESSGAP      8         /* the size of the useless gap in pixels */
+#define USELESSGAP      10         /* the size of the useless gap in pixels */
 
 /**
  * open applications to specified desktop with specified mode.
@@ -45,7 +45,7 @@ static const AppRule rules[] = { \
  * custom commands
  * must always end with ', NULL };'
  */
-static const char *termcmd[] = { "xterm",     NULL };
+static const char *termcmd[] = { "terminator",     NULL };
 static const char *menucmd[] = { "dmenu_run", NULL };
 
 #define DESKTOPCHANGE(K,N) \
@@ -60,7 +60,7 @@ static Key keys[] = {
     {  MOD1,             XK_b,          togglepanel,       {NULL}},
     {  MOD1,             XK_BackSpace,  focusurgent,       {NULL}},
     {  MOD1|SHIFT,       XK_c,          killclient,        {NULL}},
-    {  MOD4,             XK_c,          centerwindow,      {NULL}},
+    {  MOD1,             XK_c,          centerwindow,      {NULL}},
     {  MOD1,             XK_j,          next_win,          {NULL}},
     {  MOD1,             XK_k,          prev_win,          {NULL}},
     {  MOD1,             XK_h,          resize_master,     {.i = -10}}, /* decrease size in px */
@@ -81,10 +81,10 @@ static Key keys[] = {
     {  MOD1|SHIFT,       XK_g,          switch_mode,       {.i = GRID}},
     {  MOD1|SHIFT,       XK_f,          switch_mode,       {.i = FLOAT}},
     {  MOD1|SHIFT,       XK_i,          switch_mode,       {.i = FIBONACCI}},
-    {  MOD1|CONTROL,     XK_r,          quit,              {.i = 0}}, /* quit with exit value 0 */
+    {  MOD1|SHIFT,       XK_q,          quit,              {.i = 0}}, /* quit with exit value 0 */
     {  MOD1|CONTROL,     XK_q,          quit,              {.i = 1}}, /* quit with exit value 1 */
-    {  MOD1|SHIFT,       XK_Return,     spawn,             {.com = termcmd}},
-    {  MOD4,             XK_v,          spawn,             {.com = menucmd}},
+    {  MOD1,       XK_Return,     spawn,             {.com = termcmd}},
+    {  MOD1,             XK_p,          spawn,             {.com = menucmd}},
     {  MOD4,             XK_j,          moveresize,        {.v = (int []){   0,  25,   0,   0 }}}, /* move down  */
     {  MOD4,             XK_k,          moveresize,        {.v = (int []){   0, -25,   0,   0 }}}, /* move up    */
     {  MOD4,             XK_l,          moveresize,        {.v = (int []){  25,   0,   0,   0 }}}, /* move right */
@@ -93,10 +93,10 @@ static Key keys[] = {
     {  MOD4|SHIFT,       XK_k,          moveresize,        {.v = (int []){   0,   0,   0, -25 }}}, /* height shrink */
     {  MOD4|SHIFT,       XK_l,          moveresize,        {.v = (int []){   0,   0,  25,   0 }}}, /* width grow    */
     {  MOD4|SHIFT,       XK_h,          moveresize,        {.v = (int []){   0,   0, -25,   0 }}}, /* width shrink  */
-       DESKTOPCHANGE(    XK_F1,                             0)
-       DESKTOPCHANGE(    XK_F2,                             1)
-       DESKTOPCHANGE(    XK_F3,                             2)
-       DESKTOPCHANGE(    XK_F4,                             3)
+       DESKTOPCHANGE(    XK_1,                             0)
+       DESKTOPCHANGE(    XK_2,                             1)
+       DESKTOPCHANGE(    XK_3,                             2)
+       DESKTOPCHANGE(    XK_4,                             3)
 };
 
 /**
---
+++ startup.sh
@@ -0,0 +1,3 @@
+picom &
+nitrogen --set-scaled "/usr/share/backgrounds/Black_hole_by_Marek_Koteluk.jpg"
+/usr/local/bin/battery_alert 10 &
